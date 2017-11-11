cdb806a4f9758603db79d0a12cb61440e1d89b09
Keiichiro Miyamoto <miyamotok0105@gmail.com>
2017-11-11 13:08:26 +0900

    ios opencv

---
+++ tutorial/opencv_ios/opencv_ios/OpenCVWrapper.mm
@@ -0,0 +1,24 @@
+//
+//  OpenCVWrapper.m
+//  opencv_ios
+//
+//  Created by USER on 2017/11/11.
+//  Copyright © 2017年 USER. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+//#import "OpenCVWrapper.h"
+#import "opencv_ios-Bridging-Header.h"
+
+#import <opencv2/opencv.hpp>
+#import <opencv2/highgui/ios.h>
+
+@implementation OpenCVWrapper
+
+- (id) init {
+    if (self = [super init]) {
+    }
+    return self;
+}
+
+@end
---
+++ tutorial/opencv_ios/opencv_ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  opencv_ios
+//
+//  Created by USER on 2017/11/10.
+//  Copyright © 2017年 USER. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
