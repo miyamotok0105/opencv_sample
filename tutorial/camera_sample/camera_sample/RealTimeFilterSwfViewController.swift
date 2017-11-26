0961a951782953ff236c0841f8e67dfcbdadd302
Keiichiro Miyamoto <miyamotok0105@gmail.com>
2017-11-27 03:15:01 +0900

    add

---
+++ tutorial/camera_sample/camera_sample/RealTimeFilterSwfViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RealTimeFilterSwfViewController.swift
+//  camera_sample
+//
+//  Created by USER on 2017/11/27.
+//  Copyright © 2017年 USER. All rights reserved.
+//
+
+import UIKit
+
+class RealTimeFilterSwfViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ tutorial/camera_sample/camera_sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  camera_sample
+//
+//  Created by USER on 2017/11/27.
+//  Copyright © 2017年 USER. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
