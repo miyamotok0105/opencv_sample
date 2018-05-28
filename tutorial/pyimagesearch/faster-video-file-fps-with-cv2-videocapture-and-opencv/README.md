1d4fe5eb1c2fb39b96e3fbf45c9fe68c83f7f8ac
miyamoto <miyamotok0105@gmail.com>
2018-05-28 14:21:51 +0900

    Add fater video capture opencv

---
+++ tutorial/pyimagesearch/faster-video-file-fps-with-cv2-videocapture-and-opencv/README.md
@@ -0,0 +1,9 @@
+
+blog post
+https://www.pyimagesearch.com/2017/02/06/faster-video-file-fps-with-cv2-videocapture-and-opencv/
+
+VideoCapture Method is very slow.
+so the practice attend queue but still slow.
+if you have logn video , you have to wait many time
+for load videocapture to queue.
+
---
+++ tutorial/pyimagesearch/faster-video-file-fps-with-cv2-videocapture-and-opencv/file_stream.py
@@ -0,0 +1,61 @@
+from threading import Thread
+import sys
+import cv2
+import queue
+import time
+
+class FileVideoStream:
+    def __init__(self, path, queueSize=50):
+        # initialize the file video stream along with the boolean
+        # used to indicate if the thread should be stopped or not
+        self.stream = cv2.VideoCapture(path)
+        self.stopped = False
+ 
+        # initialize the queue used to store frames read from
+        # the video file
+        self.Q = queue.Queue(maxsize=queueSize)
+
+    def start(self):
+        # start a thread to read frames from the file video stream
+        t = Thread(target=self.update, args=())
+        t.daemon = True
+        t.start()
+        return self
+
+    def update(self):
+        # keep looping infinitely
+        while True:
+            print("qsize:",self.Q.qsize())
+            # if the thread indicator variable is set, stop the
+            # thread
+            if self.stopped:
+                return
+            # otherwise, ensure the queue has room in it
+            if not self.Q.full():
+                # read the next frame from the file
+                (grabbed, frame) = self.stream.read()
+ 
+                # if the `grabbed` boolean is `False`, then we have
+                # reached the end of the video file
+                if not grabbed:
+                    self.stop()
+                    return
+            
+                # add the frame to the queue
+                self.Q.put(frame)
+
+    def read(self):
+        # return next frame in the queue
+        # time.sleep(0.3)
+        return self.Q.get()
+
+    def more(self):
+        if self.Q.qsize() == 0:
+            time.sleep(10)
+        # return True if there are still frames in the queue
+        return self.stopped == False
+
+    def stop(self):
+        # indicate that the thread should be stopped
+        self.stopped = True
+
